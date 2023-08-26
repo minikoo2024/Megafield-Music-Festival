6bcef8a97bc2af7e1c50f361ff0313774b0bb5f9
minikoo2024 <133376732+minikoo2024@users.noreply.github.com>
2023-08-26 11:54:30 -0700

    Update Map.jsx

--- src/page/Map.jsx
+++ src/page/Map.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import map from "../resources/map/map.jpg";
+import map from "../resources/map/Map.jpg";
+import {TransformWrapper, TransformComponent} from "react-zoom-pan-pinch";
 
 const Map = () => {
   function zoomIn() {
     // make zoom In function here
+    
   }
 
   function zoomOut() {
     // same above
+    
   }
 
   return (
     <div>
-      <h2>Map page</h2>
-      <img src={map} className="map img" alt="logo" />
+      <h2>Map</h2>
+      <TransformWrapper defaultScale={1} defaultPositionX={100} defaultPositionY={100}>
+        <TransformComponent>
+        <img src={map} className="map img" alt="logo" />
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   );
 };
